60d1d4fda1b2634769a314016a03c729e674d9e9
esa87 <sergeywork7@gmail.com>
2025-01-25 15:48:04 +0300

    hw 15-1 full

--- src/main/java/guru/qa/countries/service/CountryService.java
+++ src/main/java/guru/qa/countries/service/CountryService.java
@@ -1,6 +1,8 @@
 package guru.qa.countries.service;
 
 import guru.qa.countries.domain.Country;
+import guru.qa.countries.domain.graphql.CountryGraphql;
+import guru.qa.countries.domain.graphql.CountryInputGraphql;
 
 import java.util.List;
 import java.util.UUID;
@@ -9,9 +11,17 @@ public interface CountryService {
 
     List<Country> allCountries();
 
+    List<CountryGraphql> allCountriesGraphql();
+
     Country getCountryByName(String countryName);
+
+    CountryGraphql graphqlCountryByName(String countryName);
 
     Country createCountry(Country country);
 
+    CountryGraphql createCountryGraphql(CountryInputGraphql country);
+
     Country updateCountry(UUID id, Country country);
+
+    CountryGraphql updateCountryGraphql(UUID id, CountryInputGraphql country);
 }
